e4a31273ed5276cbcf869cffe15f437e1cf8dba9
Dropper7856 <donovan.beulze@gmail.com>
2024-09-19 22:43:25 -0400

    Ajout d'un mode bouton pour le dropdown menu (alias drole de Allan)

--- ios/Sources/IOSStatusBarPlugin/IOSStatusBarPlugin.swift
+++ ios/Sources/IOSStatusBarPlugin/IOSStatusBarPlugin.swift
@@ -34,6 +34,5 @@ public class IOSStatusBarPlugin: CAPPlugin, CAPBridgedPlugin {
         implementation.setBottomBarColor(colorHex)
         call.resolve()
     }
-    
-    //*** Terminating app due to uncaught exception 'NSInternalInconsistencyException', reason: 'App called -statusBar or -statusBarWindow on UIApplication: this code must be changed as there's no longer a status bar or status bar window. Use the statusBarManager object on the window scene instead.'
+
 }
